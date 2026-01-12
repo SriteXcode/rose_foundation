29e0b72528139c70d15df86749c570737d234cac
SriteXcode <riteshgangster420@gmail.com>
2026-01-12 16:31:54 +0530

    hello3

--- backend/config/db.js
+++ backend/config/db.js
@@ -9,7 +9,6 @@ const connectDB = async () => {
     console.log("✅ MongoDB Connected");
   } catch (err) {
     console.error("❌ MongoDB Connection Error:", err.message);
-    process.exit(1);
   }
 };
 
